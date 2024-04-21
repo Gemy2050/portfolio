e090ef774560df9176a9a5847c847fac364a6e0f
Gemy <gemy46349@gmail.com>
2024-04-21 18:06:13 +0200

    Modified

--- js/main.js
+++ js/main.js
@@ -1,82 +1,115 @@
-// On Window Scroll
-window.onscroll = ()=> {
+//* On Window Scroll
+window.onscroll = () => {
   increaseWidth();
   scrollTop();
+};
+
+// * Make Typing Effect
+
+let typingElement = document.querySelector(".intro");
+let words = ["FrontEnd Developer", "Mohamed Gamal", "21 Years Old"];
+let index = 0;
+let mainIntervalID;
+
+let typingDuration = 2000;
+let typingDelay = 2000;
+let removingDuration = 2000;
+let removeDelay = 2000;
+
+typingElement.innerText = "";
+
+function startTyping() {
+  mainIntervalID = setInterval(addWord, typingDelay);
+}
+addWord();
+
+function addWord() {
+  clearInterval(mainIntervalID);
+
+  index++;
+  if (index > words.length - 1) index = 0;
+
+  typingElement.innerText = "";
+  let i = 0;
+  let word = words[index];
+
+  let intervalID = setInterval(() => {
+    typingElement.innerHTML += word[i];
+    i++;
+
+    if (i > word.length - 1) {
+      clearInterval(intervalID);
+      setTimeout(removeWord, removeDelay);
+    }
+  }, typingDuration / word.length);
 }
 
+function removeWord() {
+  startTyping();
 
-// Typed Library
-let word = new Typed(".intro", {
-  strings: ["Mohamed Gamal",  "20 Years"],
-  startDelay: 300,
-  typeSpeed: 100,
-  backDelay: 2000,
-  backSpeed: 100,
-  loop: true,
-  cursorChar: "|",  // Default
-});
+  let word = words[index];
+  let i = word.length;
+  let intervalID = setInterval(() => {
+    i--;
+    typingElement.innerHTML = word.slice(0, i);
+    if (i == 0) {
+      clearInterval(intervalID);
+    }
+  }, removingDuration / word.length);
+}
 
-
-// Increase Width of Skills
+//* Increase Width of Skills
 function increaseWidth() {
-
   let widthSpans = document.querySelectorAll(".skills .progress span");
-  let skillsSection= document.querySelector(".skills");
+  let skillsSection = document.querySelector(".skills");
 
   if (window.scrollY >= skillsSection.offsetTop - 100) {
     widthSpans.forEach((el) => (el.style.width = el.dataset.width));
   }
 }
 
-// Scroll To Top Button
+//* Scroll To Top Button
 function scrollTop() {
-
-  if(window.scrollY >= 3000)
+  if (window.scrollY >= 3000)
     document.querySelector(".scroll-btn").style.right = "20px";
-  else 
-  document.querySelector(".scroll-btn").style.right = "-50px";
-
+  else document.querySelector(".scroll-btn").style.right = "-50px";
 }
 
-document.querySelector(".scroll-btn").onclick = ()=> {
+document.querySelector(".scroll-btn").onclick = () => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
-  })
-}
+  });
+};
 
-
-// Create Popup
+//* Create Popup
 let projectsImages = document.querySelectorAll(".project > img");
 
 projectsImages.forEach((img) => {
-  img.addEventListener("click", (e)=> {
-    let overlay = document.createElement("div")
-    overlay.className = 'popup-overlay';
+  img.addEventListener("click", (e) => {
+    let overlay = document.createElement("div");
+    overlay.className = "popup-overlay";
 
     let popup = document.createElement("div");
-    popup.className="popup";
+    popup.className = "popup";
     let img = document.createElement("img");
     let closeButton = document.createElement("span");
     img.src = e.target.src;
     closeButton.append(document.createTextNode("X"));
-    closeButton.className = 'close-button';
+    closeButton.className = "close-button";
     popup.append(img);
     popup.append(closeButton);
     document.body.append(overlay);
     document.body.append(popup);
-    
-  })
+  });
 });
 
-document.addEventListener("click", function(e) {
-  if(e.target.className == "close-button") {
+document.addEventListener("click", function (e) {
+  if (e.target.className == "close-button") {
     e.target.parentNode.remove();
     document.querySelector(".popup-overlay").remove();
   }
 });
-
-
 
 // Handle Contact Form With Email JS
 // let from_name = document.querySelector(".contact form .from_name")
@@ -96,7 +129,7 @@ document.addEventListener("click", function(e) {
 //     }).then((res) => {
 //       document.querySelector(".contact .email-popup").style.display="block";
 //       document.querySelector(".contact form .reset").click();
-      
+
 //       document.querySelector(".contact .email-popup span").onclick = ()=> {
 //         document.querySelector(".contact .email-popup").style.display="none";
 //       }
@@ -108,104 +141,114 @@ document.addEventListener("click", function(e) {
 // }
 
 // document.querySelector(".submit").addEventListener("click", send);
-document.querySelector(".submit").addEventListener("click", ()=> {
-  swal("Something went wrong", 'This service is not available now', 'error')
+document.querySelector(".submit").addEventListener("click", () => {
+  // alert("This service is not available now");
 });
 
+//* Get Current Year in Footer
+document.querySelector(".footer .copyright .year").innerHTML =
+  new Date().getFullYear();
 
+//* Get Projects Then Add it To Page
 
-// Get Current Year For CopyRight
-document.querySelector(".footer .copyright .year").innerHTML = new Date().getFullYear();
+async function addProjects() {
+  let projects = document.querySelector(".projects .container .row");
+  let res = await fetch("../projects.json");
+  let data = await res.json();
 
+  console.log(data);
 
+  projects.innerHTML = "";
 
-// Initial limit of projects to display
-let limit = 3;
-if(screen.width > 767) {
-  limit = 6;
-}
-displayProjects(limit);
-
-// "Show More" button
-const showMoreButton = document.querySelector(".show-more");
-showMoreButton.addEventListener("click", () => {
-  if(showMoreButton.innerHTML != "Show More") {
-    displayProjects(6);
-    return false;
-  }
-    limit += 3;
-    displayProjects(limit);
+  data.forEach((el) => {
+    projects.innerHTML += `
+      <div class="col-md-6 col-lg-4">
+          <div class="project card" data-aos="flip-left">
+            <img loading="lazy" src="${
+              el.image
+            }" class="card-img-top" alt="image">
+            <div class="card-body">
+              <h5 class="card-title text-primary">${el.title}</h5>
+              <p class="card-text">${el.description}</p>
+              <div class="links d-flex justify-content-between align-items-center">
+                <a href="${el.code}" target="_blank" title="View Code">
+                ${
+                  el.code
+                    ? '<img loading="lazy" src="imgs/github.png" class="img-fluid" alt="image">'
+                    : ""
+                }
+                </a>
+                <a href="${
+                  el.demo
+                }" target="_blank" title="Live Demo"><img loading="lazy"
+                    src="imgs/eye.png" class="img-fluid" alt="image"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
   });
-  
-  function displayProjects(num) {
-  
-  let projects = document.querySelectorAll(".projects .project");
-
-  projects.forEach((project) => project.parentElement.style.display = "none")
-  
-  for(let i=0; i<num; i++) {
-    if(i > projects.length-1) {
-      // showMoreButton.style.display="none";
-      showMoreButton.innerHTML = "Show Less";
-      break;
-    } 
-    else
-      projects[i].parentElement.style.display="block";
-  }
-
 }
 
+addProjects();
 
-// Open and Close Setting Box
+//* Open and Close Setting Box
 let icon = document.querySelector(".toggle-setting");
 let settingBox = document.querySelector(".setting-box");
 
-
-icon.onclick = function() {
+icon.onclick = function () {
   this.querySelector("svg").classList.toggle("fa-spin");
   settingBox.classList.toggle("open");
-} 
+};
 
-document.querySelectorAll("body > div:not(.setting-box)").forEach(el=> {
-  el.onclick = function() {
+document.querySelectorAll("body > div:not(.setting-box)").forEach((el) => {
+  el.onclick = function () {
     settingBox.classList.remove("open");
-    document.querySelector(".toggle-setting .fa-gear").classList.remove("fa-spin");
-  }
+    document
+      .querySelector(".toggle-setting .fa-gear")
+      .classList.remove("fa-spin");
+  };
 });
 
-
-// Handle Colors option
+//* Handle Colors option
 let colors = document.querySelectorAll(".colors-list li");
 
 colors.forEach((color) => {
   color.addEventListener("click", (e) => {
     document.querySelector("li.active").classList.remove("active");
     e.target.classList.add("active");
-    document.documentElement.style.setProperty("--red-color", e.target.dataset.color)
+    document.documentElement.style.setProperty(
+      "--red-color",
+      e.target.dataset.color
+    );
     localStorage.setItem("color", e.target.dataset.color);
-  })
+  });
 });
 
-if(localStorage.getItem("color")) {
-  document.documentElement.style.setProperty("--red-color", localStorage.getItem("color"))
+if (localStorage.getItem("color")) {
+  document.documentElement.style.setProperty(
+    "--red-color",
+    localStorage.getItem("color")
+  );
   colors.forEach((color) => {
     color.classList.remove("active");
-    if(color.dataset.color == localStorage.getItem("color")) {
+    if (color.dataset.color == localStorage.getItem("color")) {
       color.classList.add("active");
     }
-  })
+  });
 }
 
-
-// Handle ScrollBar
+//* Handle ScrollBar
 let enableScroll = document.querySelector(".scroll-bar .yes");
 let disableScroll = document.querySelector(".scroll-bar .no");
 
 document.querySelectorAll(".scroll-bar span").forEach((el) => {
-  el.addEventListener("click", (e)=>{
-    document.querySelector(".scroll-bar .selected").classList.remove("selected");
+  el.addEventListener("click", (e) => {
+    document
+      .querySelector(".scroll-bar .selected")
+      .classList.remove("selected");
     e.target.classList.add("selected");
-    if(enableScroll.classList.contains("selected")) {
+    if (enableScroll.classList.contains("selected")) {
       window.localStorage.setItem("scroll", true);
     } else {
       window.localStorage.setItem("scroll", false);
@@ -215,13 +258,14 @@ document.querySelectorAll(".scroll-bar span").forEach((el) => {
 });
 
 function scrollBar() {
-
   let scroller = document.querySelector(".scroller");
-  
-  window.addEventListener("scroll", ()=> {
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  window.addEventListener("scroll", () => {
+    let height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
     let scrollTop = document.documentElement.scrollTop;
-    scroller.style.width = `${(scrollTop / height) * 100}%`
+    scroller.style.width = `${(scrollTop / height) * 100}%`;
   });
 
   if (window.localStorage.getItem("scroll") == "true") {
@@ -233,13 +277,11 @@ function scrollBar() {
     enableScroll.classList.remove("selected");
     disableScroll.classList.add("selected");
   }
-
 }
 scrollBar();
 
-
-// Reset Options
-document.querySelector(".reset").onclick = ()=> {
+//* Reset Options
+document.querySelector(".reset").onclick = () => {
   localStorage.clear();
   location.reload();
-}
+};
